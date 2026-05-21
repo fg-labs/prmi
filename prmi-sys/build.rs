ae95948af6019a40de428767d36e8af2c7f0412e
Nils Homer <nilshomer@gmail.com>
2026-05-21 16:59:56 -0700

    build(prmi-sys): wire cbindgen build.rs to emit include/prmi.h

--- prmi-sys/build.rs
+++ prmi-sys/build.rs
@@ -1,4 +1,24 @@
+// Copyright (C) 2026 Fulcrum Genomics LLC
+// SPDX-License-Identifier: MIT
+
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     println!("cargo:rerun-if-changed=src");
-    // TODO(Task 24): invoke cbindgen here to emit include/prmi.h
+    println!("cargo:rerun-if-changed=cbindgen.toml");
+
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = PathBuf::from(&crate_dir).join("include");
+    std::fs::create_dir_all(&out_dir).expect("create include dir");
+
+    let config_path = PathBuf::from(&crate_dir).join("cbindgen.toml");
+    let config = cbindgen::Config::from_file(&config_path).expect("load cbindgen.toml");
+
+    cbindgen::Builder::new()
+        .with_crate(&crate_dir)
+        .with_config(config)
+        .generate()
+        .expect("cbindgen generate")
+        .write_to_file(out_dir.join("prmi.h"));
 }
